8e1351a8eabd447caad171dc29754d7c20f1db69
bazsup <supawit.ruen@mail.kmutt.ac.th>
2019-01-10 10:51:53 +0700

    :sparkles: add twitter smart-component
    Signed-off-by: bazsup <supawit.ruen@mail.kmutt.ac.th>

---
+++ src/components/Twitter.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import uuidv4 from 'uuid/v4'
+
+class Twitter extends Component {
+    state = {
+        text: '',
+        tweetList: [
+            {   
+                id: 1,
+                tweet: 'First tweet'
+            }
+        ]
+    }
+
+    handleTextChange = (event) => {
+        const text = event.target.value
+        this.setState({ text })
+    }
+
+    addTweet = (event) => {
+        event.preventDefault()
+        if (this.state.text !== '') {
+            const newTweet = {
+                id: uuidv4(),
+                tweet: this.state.text
+            }
+            const tweetList = [ newTweet, ...this.state.tweetList ]
+            this.setState({
+                text: '',
+                tweetList
+            })
+        }
+    }
+
+    remove = (tweetId) => {
+        const filterdTweetList = this.state.tweetList.filter(tweetItem => {
+            return tweetItem.id !== tweetId
+        })
+        this.setState({ tweetList: filterdTweetList })
+    }
+
+    render() {
+        return (
+            <div>
+                <form className="tweet-form" onSubmit={this.addTweet}>
+                    <input
+                        className="form-control"
+                        onChange={this.handleTextChange}
+                        value={this.state.text}
+                        placeholder="What happen ?"
+                    />
+                    <button className="button-tweet">tweet</button>
+                </form>
+                <ol className="tweet-list">
+                    {this.state.tweetList.map(tweetItem => (
+                        <li key={tweetItem.id} className="tweet-item">
+                            <div>
+                                {tweetItem.tweet}
+                            </div>
+                            <button className="remove-tweet" onClick={() => this.remove(tweetItem.id)}>&times;</button>
+                        </li>
+                    ))}
+                </ol>
+            </div>
+        )
+    }
+}
+
+export default Twitter
